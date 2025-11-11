4c67048c34416eafd7556e5afcce843e41303273
Antonio Jurado <antoniojuradoferreira@gmail.com>
2025-11-11 13:02:15 +0100

    Ejercicio1

--- src/app/RequestTracker.js
+++ src/app/RequestTracker.js
@@ -1,26 +1,26 @@
 import { useState } from 'react';
 
 export default function RequestTracker() {
-  const [pending, setPending] = useState(0);
-  const [completed, setCompleted] = useState(0);
+  const [pendiente, setPending] = useState(0);
+  const [completado, setCompleted] = useState(0);
 
   async function handleClick() {
-    setPending(pending + 1);
+    setPending(p => p + 1);
     await delay(3000);
-    setPending(pending - 1);
-    setCompleted(completed + 1);
+    setPending(p => p - 1);
+    setCompleted(c => c + 1);
   }
 
   return (
     <>
       <h3>
-        Pendiente: {pending}
+        Pendiente: {pendiente}
       </h3>
       <h3>
-        Completado: {completed}
+        Completado: {completado}
       </h3>
       <button onClick={handleClick}>
-        Comprar     
+        Comprar
       </button>
     </>
   );
